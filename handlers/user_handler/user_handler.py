1c85983b1cfe585fe32927433c4f33de8f3f147c
Leraner <legend.tony@yandex.ru>
2023-08-19 20:49:56 +0300

    Implemented deleting unused messages, redone alembic

--- handlers/user_handler/user_handler.py
+++ handlers/user_handler/user_handler.py
@@ -1,8 +1,10 @@
+import json
+
 from aiogram import types
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from actions.user_actions.user_actions import UserActions
-from loader import dp
+from loader import dp, redis_cache
 
 
 @dp.message_handler(commands=['start'])
@@ -11,4 +13,7 @@ async def start_command(message: types.Message, session: AsyncSession) -> None:
     user = await UserActions.get_user_by_username(username=message.from_user.username, session=session)
     if not user:
         new_user = await UserActions.create_new_user(message=message, session=session)
-        await message.answer(f'Hello, {new_user.first_name}, welcome to {bot_data["username"]}')
+        msg = await message.answer(f'Hello, {new_user.first_name}, welcome to {bot_data["username"]}')
+        useless_messages = json.loads(await redis_cache.get(message.from_user.username + ':useless_messages'))
+        useless_messages.append(msg.message_id)
+        await redis_cache.set(message.from_user.username + ':useless_messages', json.loads(useless_messages))
