43fb8c3d383ee5659754d0d94e0fc6f9bfc7e7ee
Leraner <legend.tony@yandex.ru>
2023-08-23 23:09:42 +0300

    Fixed by PEP8

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -1,5 +1,6 @@
 class PermissionDenied(Exception):
     """Exception if user hasn't enough privileges to do something"""
+
     def __init__(self, message='Permission denied') -> None:
         self.message = message
         super().__init__(self.message)
@@ -7,6 +8,7 @@ class PermissionDenied(Exception):
 
 class SerializerValidationError(Exception):
     """Exception for validation error, if no such field exists or validation error"""
+
     def __init__(self, field: str = '', message='- no such field exists') -> None:
         self.message = field + message
         super().__init__(self.message)
